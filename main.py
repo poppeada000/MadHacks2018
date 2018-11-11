c15848d27fd0364ee74a9cb4817f11576a547670
tbindokas13 <tbindokas13@gmail.com>
2018-11-11 08:37:22 -0600

    Added a few comments

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
+#Classes Imported
 import CameraDisplay
 import Clarifmain
 
+
+#Starts the Camera
 CameraDisplay.startCam()
 
+#Send data to the database to check what it is
 Clarifmain.getSoda()
