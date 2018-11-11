31610947c76a4281f318209499ec872ede069070
tbindokas13 <tbindokas13@gmail.com>
2018-11-11 08:25:27 -0600

    small change

--- car_recgonition.py
+++ car_recgonition.py
@@ -2,24 +2,14 @@
 
 import cv2
 import numpy as np
-#import matplotlib.pyplot as plt
 import time
 
 def startCam():
 cap = cv2.VideoCapture(1)
 
-bgdModel = np.zeros((1,65),np.float64)
-fgdModel = np.zeros((1,65),np.float64)
-
-
 while True:
-
     ret, img = cap.read()
-
-
-    #cv2.rectangle(img,(200,150),(900,950),(0,0,255),5)
     cv2.imshow('lot',img)
-
     for i in range(4):
         return_value, image = cap.read()
         cv2.imwrite('opencv'+str(i)+'.jpg',image)
