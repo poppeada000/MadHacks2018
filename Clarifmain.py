29ab4902e89d804c15f3a3adff13c158dec9571a
tbindokas13 <tbindokas13@gmail.com>
2018-11-11 05:45:50 -0600

    Clarifai API recognision and learning added

---
+++ Clarifmain.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os
+import numpy as np
+import cv2
+
+from clarifai.rest import ClarifaiApp
+from clarifai.rest import Image as ClImage
+
+#APi Key for Clarifai
+app = ClarifaiApp(api_key='cd72b128d29e43b39d9f932784380aa7')
+
+
+model = app.models.get('MadHacks2018')
+image = ClImage(file_obj=open('opencv0.jpg', 'rb'))
+response=model.predict([image])
+
+concepts = response['outputs'][0]['data']['concepts']
+for concept in concepts:
+    print(concept['name'], concept['value'])
+
+
--- image_brake.py
+++ image_brake.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+
+
 def main():
     img = cv2.imread('opencv0.jpg')
     mask = np.zeros(img.shape[:2],np.uint8)
@@ -8,7 +12,7 @@ def main():
     bgdModel = np.zeros((1,65),np.float64)
     fgdModel = np.zeros((1,65),np.float64)
 
-    rect = (200,150,215,200)
+    rect = (200,150,900,950)
 
     cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
     mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
