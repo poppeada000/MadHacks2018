5d628874126a08b9732f65c7ba49bfbf61d8eb93
tbindokas13 <tbindokas13@gmail.com>
2018-11-11 08:33:08 -0600

    Deleting Image break script

--- car_recgonition.py
+++ car_recgonition.py
@@ -5,17 +5,17 @@ import numpy as np
 import time
 
 def startCam():
-cap = cv2.VideoCapture(1)
+	cap = cv2.VideoCapture(1)
 
-while True:
-    ret, img = cap.read()
-    cv2.imshow('lot',img)
-    for i in range(4):
-        return_value, image = cap.read()
-        cv2.imwrite('opencv'+str(i)+'.jpg',image)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+	while True:
+    		ret, img = cap.read()
+    		cv2.imshow('lot',img)
+    		for i in range(4):
+        		return_value, image = cap.read()
+        		cv2.imwrite('opencv'+str(i)+'.jpg',image)
+    		if cv2.waitKey(1) & 0xFF == ord('q'):
+        		break
+	cap.release()
+	cv2.destroyAllWindows()
 
 return
